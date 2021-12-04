d709492f472623ac5abf2408e5817b2174acd0db
mati <matimon@frba.utn.edu.ar>
2021-12-04 17:05:12 -0300

    project initialized with spring initializr

---
+++ src/main/java/com/ensolvers/webexercise/WebexerciseApplication.java
@@ -0,0 +1,13 @@
+package com.ensolvers.webexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebexerciseApplication.class, args);
+	}
+
+}
