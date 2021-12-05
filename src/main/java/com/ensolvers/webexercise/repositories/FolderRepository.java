56418ad0ff6dad115ae39072ab85939658a5467c
mati <matimon@frba.utn.edu.ar>
2021-12-05 17:12:38 -0300

    repositories

--- src/main/java/com/ensolvers/webexercise/repositories/FolderRepository.java
+++ src/main/java/com/ensolvers/webexercise/repositories/FolderRepository.java
@@ -1,5 +1,29 @@
 package com.ensolvers.webexercise.repositories;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ensolvers.webexercise.domain.Folder;
+
 public class FolderRepository {
+	private List<Folder> folderList;
+
+	public FolderRepository() { // implementar singleton
+		this.folderList = new ArrayList<Folder>();
+	}
+
+	public List<Folder> getFolderList() {
+		return folderList;
+	}
+	
+	public void add(Folder folder) {
+		this.folderList.add(folder);
+	}
+	
+	public void remove(Folder folder) {
+		if(!this.folderList.remove(folder)) {
+			throw new RuntimeException("no existe la carpeta.");
+		}
+	}
 
 }
--- src/main/java/com/ensolvers/webexercise/repositories/UserRepository.java
+++ src/main/java/com/ensolvers/webexercise/repositories/UserRepository.java
@@ -1,5 +1,26 @@
 package com.ensolvers.webexercise.repositories;
 
+import java.util.List;
+
+import com.ensolvers.webexercise.domain.User;
+
 public class UserRepository {
+	private List<User> userList;
+
+	public UserRepository() {// implementar singleton
+	}
+
+	public List<User> getUserList() {
+		return userList;
+	}
+	public void add(User user) {
+		this.userList.add(user);
+	}
+	
+	public void remove(User user) {
+		if(!this.userList.remove(user)) {
+			throw new RuntimeException("no existe el usuario.");
+		}
+	}
 
 }
